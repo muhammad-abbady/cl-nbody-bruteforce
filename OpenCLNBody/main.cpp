012d61093d8ffa877b59f76d45145e2d9d6fc94a
muhammad-abbady <Muhammad Abbady>
2014-12-28 04:30:17 +0200

    Initial Commit

---
+++ OpenCLNBody/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCLNBody
+//
+//  Created by Muhammad Abbady on 12/28/14.
+//  Copyright (c) 2014 Muhammad Abbady. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
